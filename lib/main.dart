ad261ee7ba089f7e36499abdd307458435ed986a
indieka900 <indiekaj@gmail.com>
2022-10-07 10:40:32 +0300

    flutter application

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+//import 'package:flutter/src/material/colors.dart';
 import 'dart:math';
 import 'dart:core';
+import 'second_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -14,7 +16,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo By Joseph',
       theme: ThemeData(
-        backgroundColor: Colors.pink,
+        backgroundColor: const Color.fromARGB(255, 80, 28, 45),
         primarySwatch: Colors.brown,
       ),
       debugShowCheckedModeBanner: false,
@@ -73,13 +75,15 @@ class _AppextState extends State<Appext> {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.tittle),
+        backgroundColor: const Color.fromARGB(213, 31, 17, 233),
+        shadowColor: const Color.fromARGB(255, 94, 7, 1),
       ),
       body: Center(
         child: currentindex == 0
             ? Container(
                 width: double.infinity,
                 height: double.infinity,
-                color: Colors.grey,
+                color: const Color.fromARGB(255, 209, 157, 157),
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   crossAxisAlignment: CrossAxisAlignment.start,
@@ -102,13 +106,17 @@ class _AppextState extends State<Appext> {
                     ),
                     ElevatedButton(
                       style: ElevatedButton.styleFrom(
-                          onPrimary: Colors.blue,
-                          primary: Colors.black87,
-                          shadowColor: Colors.deepOrange),
+                        onPrimary: const Color.fromARGB(255, 89, 166, 230),
+                        primary: const Color.fromARGB(221, 38, 34, 49),
+                        shadowColor: const Color.fromARGB(255, 47, 24, 73),
+                      ),
                       onPressed: _incrementCounter,
                       child: const Text("Increase"),
                     ),
                     ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        onPrimary: Color.fromARGB(233, 175, 228, 156),
+                      ),
                       onPressed: () {
                         Navigator.of(context).push(
                           MaterialPageRoute(
@@ -136,7 +144,7 @@ class _AppextState extends State<Appext> {
       floatingActionButton: FloatingActionButton(
         onPressed: generate,
         tooltip: 'Increment',
-        backgroundColor: Colors.green,
+        backgroundColor: const Color.fromARGB(172, 44, 133, 22),
         child: const Icon(Icons.add_alert),
       ), // This trailing comma makes auto-formatting nicer for build methods.
       // bottomNavigationBar: FloatingActionButton(
@@ -161,7 +169,7 @@ class _AppextState extends State<Appext> {
           BottomNavigationBarItem(
             icon: Icon(Icons.policy),
             label: "Terms & Condition",
-            backgroundColor: Colors.purple,
+            backgroundColor: Color.fromARGB(255, 112, 18, 70),
           ),
         ],
         currentIndex: currentindex,
@@ -171,23 +179,10 @@ class _AppextState extends State<Appext> {
           });
         },
         iconSize: 35,
-        fixedColor: Colors.red,
-        backgroundColor: Colors.white24,
-        unselectedItemColor: Colors.deepPurple,
+        fixedColor: const Color.fromARGB(255, 209, 10, 43),
+        backgroundColor: const Color.fromARGB(60, 54, 53, 53),
+        unselectedItemColor: const Color.fromARGB(255, 83, 14, 204),
       ),
-    );
-  }
-}
-
-class SecondPage extends StatelessWidget {
-  const SecondPage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-          //title: Text(widget.title),
-          ),
     );
   }
 }
